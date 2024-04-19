acbc92592a233880cb6cb36f07089a74f5e43a38
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-19 11:39:48 -0300

    feat: Cria arquivo para as exceptions

--- BlogApi/src/main/java/com/example/BlogApi/excepctions/ResourceNotFoundException.java
+++ BlogApi/src/main/java/com/example/BlogApi/excepctions/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-package com.example.BlogApi.excepctions;public class ResourceNotFoundException {
+package com.example.BlogApi.excepctions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
