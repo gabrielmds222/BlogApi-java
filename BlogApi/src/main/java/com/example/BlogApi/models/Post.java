0278e90a207a4ad5ea4702ca0c8d520bb8b853a5
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-19 11:35:37 -0300

    feat: Cria model dos posts

---
+++ BlogApi/src/main/java/com/example/BlogApi/models/Post.java
@@ -0,0 +1,31 @@
+package com.example.BlogApi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
+    private BigDecimal preco;
+}
