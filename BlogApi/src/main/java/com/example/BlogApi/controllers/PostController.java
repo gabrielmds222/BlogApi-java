4cb0cebefc433acab4ac99db1258bba55c234358
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-04-19 23:16:49 -0300

    feat: Adiciona requisições get e post

---
+++ BlogApi/src/main/java/com/example/BlogApi/controllers/PostController.java
@@ -0,0 +1,28 @@
+package com.example.BlogApi.controllers;
+
+import com.example.BlogApi.models.Post;
+import com.example.BlogApi.repositories.PostRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class PostController {
+    private PostRepository postRepository;
+
+    PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    
+    @GetMapping("/posts")
+    List<Post> all() {
+
+        return postRepository.findAll();
+    }
+
+    @PostMapping("/posts")
+    public Post adicionaPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+}
